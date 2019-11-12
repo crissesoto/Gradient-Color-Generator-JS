ccfe95e1d278d96dabc4c66c98506afadc550fcb
crissesoto <soto.crisse@hotmail.com>
2019-11-12 12:12:44 +0100

    added atch the background generated on the first page load.

--- app.js
+++ app.js
@@ -2,14 +2,25 @@
 const body = document.getElementById("background");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
-const colorTextGenerator = document.querySelector("h3");
+const newColorText = document.querySelector(".new");
+const currentColorText = document.querySelector(".current");
+const h2 = document.getElementById("h2-current");
+console.log(currentColorText);
+
+// initial color gradient text function
+const loadPage = () => {
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  currentColorText.textContent = body.style.background + ";";
+};
 
 // function gradient color generator
 const colorMixer = () => {
   body.style.background =
     "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
-  colorTextGenerator.textContent = body.style.background + ";";
+  h2.textContent = "Inital Gradient";
+  newColorText.textContent = body.style.background + ";";
 };
 
 // eventListeners input
