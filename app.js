7a6cb4ac4dbc6dc417a7fe7c9e09abfda73091e3
crissesoto <soto.crisse@hotmail.com>
2019-11-12 10:34:09 +0100

    added colorMixer function

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const colorGenerator = document.querySelector("h3");
 
+// function gradient color generator
+const colorMixer = () => {
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+};
+
 // eventListeners input
 
-color1.addEventListener("input", () => {
-  console.log(color1.value);
-});
-color2.addEventListener("input", () => {
-  console.log(color2.value);
-});
+color1.addEventListener("input", colorMixer());
+color2.addEventListener("input", colorMixer());
