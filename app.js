4cfb69573b9656937e24d5bdcf54ff4d5ff8c519
crissesoto <soto.crisse@hotmail.com>
2019-11-12 12:32:51 +0100

    random button which generates two random numbers for the colour inputs.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const color2 = document.querySelector(".color2");
 const newColorText = document.querySelector(".new");
 const currentColorText = document.querySelector(".current");
 const h2 = document.getElementById("h2-current");
-console.log(currentColorText);
+const button = document.querySelector(".btn");
 
 // initial color gradient text function
 const loadPage = () => {
@@ -27,3 +27,22 @@ const colorMixer = () => {
 
 color1.addEventListener("input", colorMixer);
 color2.addEventListener("input", colorMixer);
+
+// Random button which generates two random numbers for the colour inputs.
+
+const randomHue = () => {
+  for (i = 0; i <= 255; i++) {
+    random1 = Math.floor(Math.random() * 257);
+    random2 = Math.floor(Math.random() * 257);
+    random3 = Math.floor(Math.random() * 257);
+    const randomHue1 = `rgb(${random1}, ${random2}, ${random3})`;
+    const randomHue2 = `rgb(${random3}, ${random1}, ${random2})`;
+    body.style.background =
+      "linear-gradient(to right, " + randomHue1 + ", " + randomHue2 + ")";
+  }
+  newColorText.textContent = body.style.background + ";";
+};
+
+button.addEventListener("click", () => {
+  randomHue();
+});
