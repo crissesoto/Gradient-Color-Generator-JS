98bd50a58a34d66e3ed3438f15472e5cc5f2f417
crissesoto <soto.crisse@hotmail.com>
2019-11-12 10:59:08 +0100

    added colorTextGenerator to the function ColorMixer

--- app.js
+++ app.js
@@ -2,15 +2,17 @@
 const body = document.getElementById("background");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
-const colorGenerator = document.querySelector("h3");
+const colorTextGenerator = document.querySelector("h3");
 
 // function gradient color generator
 const colorMixer = () => {
   body.style.background =
     "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+  colorTextGenerator.textContent = body.style.background + ";";
 };
 
 // eventListeners input
 
-color1.addEventListener("input", colorMixer());
-color2.addEventListener("input", colorMixer());
+color1.addEventListener("input", colorMixer);
+color2.addEventListener("input", colorMixer);
